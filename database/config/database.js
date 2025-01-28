5600d142bfb2ab3f5ca7fd3cc474af77b41d9e39
Marcelo Evangelista <tancker@hotmail.com>
2025-01-28 16:13:15 -0300

    iniciando novamente o projeto

--- database/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: process.env.DB_NAME
-});
-
-module.exports = sequelize;
